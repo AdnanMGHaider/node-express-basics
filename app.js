d6fa51023bf3eee59aad382c40617d87b85bc0ae
Adnan Haider <adnan.mgh92@gmail.com>
2023-03-30 17:30:38 +0600

    Organize static files, add CSS links, and serve static files
    - Create public folder and CSS files:
      * Create 'public' folder in the root directory
      * Inside 'public', create 'css' folder
      * Inside 'css', create 'main.css' and 'product.css' files
    
    - Add styling to CSS files:
      * Add common styles to 'main.css' for 'shop.html', 'add-product.html', and '404.html' (header and navigation styling)
      * Add specific styles to 'product.css' for 'add-product.html'
    
    - Serve static files:
      * In 'app.js', register a new middleware using 'app.use()'Use the 'express.static()' method
      * to provide the path to the 'public' folder for serving CSS files statically
    
    - Add CSS links to HTML files:
      * Add links to 'main.css' in 'shop.html', 'add-product.html', and '404.html'
      * In 'add-product.html', add a link to 'product.css' in addition to 'main.css' link

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+//This is to serve the css files statically and we provide a path to the folder that contains the css file.
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
