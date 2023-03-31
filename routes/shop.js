ba488e80eccf617c15a7308a56b49eee114a63fa
Adnan Haider <adnan.mgh92@gmail.com>
2023-03-31 18:41:18 +0600

    Outputting Dynamic content in Shop.pug
    * Pass the products array data into the res.render() function
        - In the shop.js file, take the adminData object that you have imported, tap into the products object (which is an array) and save it in a constant called 'products'.
            >This should be done inside the router.get() method above the res.render() function.
        - Next as a second argument after the 'shop' string, create an object with a property:value of prods:products, and another property:value of docTitle:'Shop'. Now the value for 'prods' and 'docTitle' will be available in shop.pug.
            > The res.render() function allows you to pass in data into the template (shop.pug in our case) as a second argument, and you could use that data in the template.
    
    * Complete shop.pug markup with some dynamic content
        - Go into shop.pug and in the place where 'My Shop' was written, replace that with #{docTitle}
        - Then go ahead and complete the shop.pug markup after the already existing header element.
            > At this point the product title, img source, price value, description are all hard-coded
            > The elements after the header element include, a main element, inside that a div with a class of 'grid', inside that an article element, inside that a header element (for the title of the product) and 3 other sibling div elements for the image, price, description and button.
    
    * Iterating through the prods array and conditionally rendering array items or 'No products' message
        - In between the div element with a class of grid and the 'article' element, include the following line of code: each product in prods
            > This will iterate through the prods array and each product in each iteration will be saved in the 'product' variable
        - In the place where the hard coded product name, i.e. 'Great Book' was there, replace it with #{product.title}
            > 'product.title' because we are referring to one product in one iteration of the loop which is saved in the 'product' variable.
        - In the chance that no items are in the prods array, in between the 'main' element and the 'div' element, insert the code: if prods.length > 0
            > This is an 'if' check to see if the prods array is empty or not because if it isn't the same article elements will be shown multiple times depending on how many products are there in the array.
        And then after the last element which is a 'div' element with the class of 'card__actions', insert an 'else' keyword (this else keyword should be indented at the same level of the 'if' check above'), and then inside that insert an h1 elememnt with the message of 'No Products'.

--- routes/shop.js
+++ routes/shop.js
@@ -7,9 +7,10 @@ const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+  const products = adminData.products;
   // console.log("shop.js", adminData.products);
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
-  res.render("shop");
+  res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
